ef1b6bbaec8b751cc8cb7bb1c4b1a959de863197
VinyProgramador <vinicius.nunes@sptech.school>
2022-05-29 22:06:54 -0300

    criando modals da tela de usuario.

---
+++ prjIndividual/public/js/modals.js
@@ -0,0 +1,20 @@
+function modalInfo() {
+    modalInfoId.style.display = 'block';
+    modalInfoId.style.transition = '500ms';
+
+}
+
+function fecharModalInfo() {
+    modalInfoId.style.display = 'none';
+
+}
+
+function modalConfig() {
+    modalConfigId.style.display = 'block';
+
+}
+
+function fecharModalConfig() {
+    modalConfigId.style.display = 'none';
+
+}
